f6dbdac3e6015ff0fc0f410d8206fede4dab7165
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-16 22:58:39 +0300

    Update bing.py

--- DAXXMUSIC/plugins/Yumi/bing.py
+++ DAXXMUSIC/plugins/Yumi/bing.py
@@ -5,11 +5,12 @@ from pyrogram import filters
 
 API_URL = "https://sugoi-api.vercel.app/search"
 
-@app.on_message(filters.command("bingsearch"))
+@app.on_message(
+   filters.command(["بحث"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def bing_search(michiko, message):
     try:
         if len(message.command) == 1:
-            await message.reply_text("Please provide a keyword to search.")
+            await message.reply_text("🚦اكتب شيء للبحثُ عنه... ")
             return
 
         keyword = " ".join(
@@ -21,7 +22,7 @@ async def bing_search(michiko, message):
         if response.status_code == 200:
             results = response.json()
             if not results:
-                await message.reply_text("No results found.")
+                await message.reply_text("🚦لم يتم العثور على شيء... ")
             else:
                 message_text = ""
                 for result in results[:7]:
@@ -30,6 +31,6 @@ async def bing_search(michiko, message):
                     message_text += f"{title}\n{link}\n\n"
                 await message.reply_text(message_text.strip())
         else:
-            await message.reply_text("Sorry, something went wrong with the search.")
+            await message.reply_text("🚦انتظر قليلا البحث يأخذ وقت اكثر...")
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
