53126f7249c64a646e05f2f1eba4518d55958986
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-16 22:33:59 +0300

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -51,8 +51,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ALLTYPECC")
-                await self.one.join_chat("DAXXSUPPORT")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +71,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ALLTYPECC")
-                await self.one.join_chat("DAXXSUPPORT")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +91,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("ALLTYPECC")
-                await self.one.join_chat("DAXXSUPPORT")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +111,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ALLTYPECC")
-                await self.one.join_chat("DAXXSUPPORT")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +131,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("ALLTYPECC")
-                await self.one.join_chat("DAXXSUPPORT")
+                await self.one.join_chat("ah07v")
             except:
                 pass
             assistants.append(5)
