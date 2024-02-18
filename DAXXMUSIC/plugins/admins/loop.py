3e641f7a7d20437ac3c439bb824f2b8fde5b54eb
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-18 22:35:09 +0300

    Update loop.py

--- DAXXMUSIC/plugins/admins/loop.py
+++ DAXXMUSIC/plugins/admins/loop.py
@@ -8,7 +8,12 @@ from DAXXMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["loop", "cloop"]) & filters.group & ~BANNED_USERS)
+@app.on_message(
+   filters.command(["loop", "تكرار"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+            
+    & filters.group
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_17"]
