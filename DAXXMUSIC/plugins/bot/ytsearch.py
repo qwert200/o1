b3e6273750cec6fb45ea169fb938cf9b0a056bf3
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-19 09:13:29 +0300

    Update ytsearch.py

--- DAXXMUSIC/plugins/bot/ytsearch.py
+++ DAXXMUSIC/plugins/bot/ytsearch.py
@@ -9,7 +9,8 @@ from config import BOT_USERNAME
 
 
 
-@app.on_message(filters.command("search"))
+@app.on_message(
+   filters.command(["search", "بحث"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
