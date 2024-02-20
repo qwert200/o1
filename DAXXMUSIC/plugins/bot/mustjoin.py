3a8776899af584ea32998a8c7bfb9e8b4e106af1
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-20 07:49:48 +0300

    Update mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -8,7 +8,7 @@ from DAXXMUSIC import app
 
 MUST_JOIN = "ah07v"
 #------------------------
-@app.on_message(filters.regex('^/start$') & filters.private & filters.group)
+@app.on_message(filters.regex('^/start$') & filters.group)
 async def must_join_channel(app: Client, msg: Message):
     if not MUST_JOIN:
         return
