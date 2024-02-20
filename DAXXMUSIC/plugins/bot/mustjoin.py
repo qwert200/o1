cf0a9df3346855036459384b7e826f73021f9e9d
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-20 17:44:23 +0300

    Update mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -1,39 +1,27 @@
-
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardMarkup as Markup, InlineKeyboardButton as Button
+from pyrogram.enums import ChatType
+from pyrogram.errors import UserNotParticipant
 from DAXXMUSIC import app
 
-#--------------------------
+channel = "ah07v"
+async def subscription(_, __: Client, message: Message):
+    try: await app.get_chat_member(channel, user_id)
+    except UserNotParticipant: return False
+    return True
 
-MUST_JOIN = "ah07v"
-#------------------------
-@app.on_message( filters.incoming & filters.group, group=-1)
-@app.on_message( filters.incoming & filters.private, group=-1)
-async def must_join_channel(app: Client, msg: Message):
-    if not MUST_JOIN:
-        return
-    try:
-        try:
-            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
-                chat_info = await app.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
-            try:
-                await msg.reply(f"︙عـذراً، عـلـيـڪ الانـضـمـام الى هـذهِ الـقـنـاة أولاً  [ ♚...« مـكـنـوناتي 𝙷𝙼𝙳 »... ♚ ]({link}) \n︙اشـتـرڪ ثـم أرسـل : /start",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("♚...« مـكـنـوناتي 𝙷𝙼𝙳 »... ♚", url=link),
-                            ]
-                        ]
-                    )
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"ارفع البوت مشࢪف في القناة: {MUST_JOIN} !")
+subscribed = filters.create(subscription)
+
+@app.on_message(~subscribed)
+async def checker(_: Client, message: Message):
+    if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]: await message.delete()
+    user_id = message.from_user.id
+    user = message.from_user.first_name
+    markup = Markup([
+        [Button("- ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ -", url=f"https://t.me/{channel}")]
+    ])
+    await message.reply(
+        f"عذرًا عزيزي {user}عليك الإشتراك بقناة السورس أولا.",
+        reply_markup = markup
+    )
