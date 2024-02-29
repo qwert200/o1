21b5bdf805cf69259ee39f2ae427884513b63d03
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-29 17:43:25 +0300

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -26,72 +26,20 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text=_["H_B_1"],
+                    text="‹ اوامر القنوات ›",
                     callback_data="help_callback hb1",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
-                    text=_["H_B_2"],
+                    text="‹ اوامر المجموعات ›",
                     callback_data="help_callback hb2",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
-                    text=_["H_B_3"],
+                    text="‹ اوامر بالانگليزي ›",
                     callback_data="help_callback hb3",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
                 ),
             ],
             mark,
@@ -105,8 +53,11 @@ def help_back_markup(_):
         [
             [
                 InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
+                    text="‹ اوامر التشغيل ›",
                     callback_data=f"settings_back_helper",
+                ),
+                InlineKeyboardButton(
+                    text="‹ القائمة الرئيسية ›", callback_data=f"settingsback_helper"
                 ),
             ]
         ]
@@ -118,8 +69,8 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_4"],
-                url=f"https://t.me/{app.username}?start=help",
+                text="‹ اوامر التشغيل ›",
+                callback_data="settings_back_helper",
             ),
         ],
     ]
