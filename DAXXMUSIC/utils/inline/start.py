dde8a98238578391f77d74b662e1bdc15c326eba
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-29 17:39:20 +0300

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -19,17 +19,15 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+            InlineKeyboardButton(text="‹ اوامر التشغيل ›", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="‹ طريقه التفعيل ›", callback_data="help_callback hb6"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="‹ المطور ›", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="‹ قناة البوت ›", url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            text="‹ اضف البوت لمجموعتك ›", url=f"https://t.me/{app.username}?startgroup=true"),
         ],
     ]
     return buttons
