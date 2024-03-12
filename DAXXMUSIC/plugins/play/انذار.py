a32a19eb38dac5bd602ff15ca4be78aa01a094ad
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-12 14:18:36 +0300

    Update انذار.py

--- DAXXMUSIC/plugins/play/انذار.py
+++ DAXXMUSIC/plugins/play/انذار.py
@@ -7,8 +7,8 @@ from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-from HamodyMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from HamodyMusic import app
+from DAXXMUSIC import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from DAXXMUSIC import app
 from asyncio import gather
 from pyrogram.errors import FloodWait
 
