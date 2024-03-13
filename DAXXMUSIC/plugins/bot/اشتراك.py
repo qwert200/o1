01ad13eb49374ecba404ff9df20aa1df56da8cdf
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-13 22:03:39 +0300

    Update اشتراك.py

--- DAXXMUSIC/plugins/bot/اشتراك.py
+++ DAXXMUSIC/plugins/bot/اشتراك.py
@@ -11,7 +11,7 @@ from config import BANNED_USERS, lyrical
 MUST_JOIN = "ah07v"
 #------------------------
 @app.on_message( filters.incoming & filters.private, group=-1)
-@app.on_message(~filters.edited & filters.incoming & filters.group, group=-1)
+@app.on_message( filters.incoming & filters.group, group=-1)
 async def must_join_channel(app: Client, msg: Message):
     if not MUST_JOIN:
         return
