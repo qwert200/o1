1bbf0a2b54609568c96169d2fcd6d5eb6101b7a6
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-14 01:44:24 +0300

    Update منوعات.py

--- DAXXMUSIC/plugins/play/منوعات.py
+++ DAXXMUSIC/plugins/play/منوعات.py
@@ -16,6 +16,21 @@ from DAXXMUSIC import app
 from random import  choice, randint
 
 
+@app.on_message(command([f"شعر", "شع", "ش", "{BOT_USERNAME} شعر"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(8,20)
+    url = f"https://t.me/saresnx/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الشعر لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+
 
 @app.on_message(command(["غنيلي", "‹ غنيلي ›", "غ", ""]))
 async def ihd(client: Client, message: Message):
