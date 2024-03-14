8b356751052cb805a6906a028ffaed09c155ec28
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-14 04:22:23 +0300

    Update شخصيه.py

--- DAXXMUSIC/plugins/play/شخصيه.py
+++ DAXXMUSIC/plugins/play/شخصيه.py
@@ -6,8 +6,8 @@ import aiohttp
 from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from HamodyMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from HamodyMusic import app
+from DAXXMUSIC import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from DAXXMUSIC import app
 from asyncio import gather
 
 
